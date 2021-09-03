763c29318b2b7738bbac281ae56b6336c9709f68
Panda <Sarita_P@Dellteam.com>
2021-09-03 09:36:09 +0530

    rendering elements

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyElement from './rendering-element';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App message="Welcome Message" />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App message="Welcome Message" />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+ReactDOM.render(<MyElement/>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/rendering-element.js
@@ -0,0 +1,5 @@
+const MyElement = ()=>{
+    return <h1> Hello World {new Date().toLocaleTimeString()}</h1>
+}
+
+export default MyElement
