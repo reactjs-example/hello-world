c67eba47bb630ae91fb6618748da5c8866207126
Panda <Sarita_P@Dellteam.com>
2021-09-05 21:21:51 +0530

    event-handler with out set state

--- src/render.js
+++ src/render.js
@@ -10,47 +10,49 @@ class LoggingButton extends React.Component {
         super(props);
         this.state = {
             message: "ON",
-            isToggleOn: true,
+            isToggleOn: true
         }
 
         console.log("OK OK");
 
     }
+
     handleClick = () => {
         console.log('this is:', this.state.isToggleOn);
         //this.state={message:"ON"}
         //this.handleClick = this.handleClick.bind(this);
         this.state.isToggleOn = !this.state.isToggleOn
-        if(this.state.isToggleOn)
-        {
+        if (this.state.isToggleOn) {
             console.log("here")
-         this.state.message ="ON";   
-         console.log(this.state)
+            this.state.message = "ON";
+            console.log(this.state)
         }
-        else{
+        else {
             console.log("here")
-         this.state.message ="OFF";   
-         console.log(this.state)
+            this.state.message = "OFF";
+            console.log(this.state)
         }
-       // this.state.message = this.state.isToggleOn ? "ON" : "OFF"
+        // this.state.message = this.state.isToggleOn ? "ON" : "OFF"
 
-    //   return render();
-    this.forceUpdate();
-       /*
-        this.setState(currentState => (
-            (console.log(currentState),{
-                    isToggleOn: !currentState.isToggleOn,
-                    message: !this.state.isToggleOn ? "ON" : "OFF"
-                }))
-        
-        )*/
+        //   return render();
+        this.forceUpdate();
+        /*
+         this.setState(currentState => (
+             (console.log(currentState),{
+                     isToggleOn: !currentState.isToggleOn,
+                     message: !this.state.isToggleOn ? "ON" : "OFF"
+                 }))
+         
+         )*/
     }
 
     render() {
         return (
-            <button onClick={this.handleClick}>
-                {this.state.message}
-            </button>
+            <div>
+                <button onClick={this.handleClick}>
+                    {this.state.message}
+                </button>
+            </div>
         );
     }
 }
