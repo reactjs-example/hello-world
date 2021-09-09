34d8ea1802a73213968cfc69da6775edff6ee2d5
r0p06np <rabindra.kumar.patra@walmart.com>
2021-09-09 17:16:18 +0530

    capturing onChange with useState and display them

--- src/components/expenses/ExpenseForm.js
+++ src/components/expenses/ExpenseForm.js
@@ -1,27 +1,43 @@
 
-
+import { useState } from "react";
 export function ExpenseForm() {
 
     const expenseFormHandler = () => {
         console.log("here")
     }
 
-    const titleChangeHandler = (event) =>{
+    const [title, updateTitle] = useState('');
+    const [amount, updateAmount] = useState('');
+    const [date, updateDate] = useState('');
+
+    const titleChangeHandler = (event) => {
         console.log(event.target.value);
-    }
+        updateTitle(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        console.log(event.target.value);
+        updateDate(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        console.log(event.target.value);
+        updateAmount(event.target.value);
+    };
     return (
         <div>
+            {title} | {amount} | {date}
             <form onSubmit={expenseFormHandler}>
                 <div className="form-control">
-                    <label>Title</label> 
-                     <input type="text" onChange={titleChangeHandler}  />
+                    <label>Title</label>
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
 
                 <div className="form-control">
-                    <label>Amount</label>  <input type="number" />
+                    <label>Amount</label>  <input type="number" onChange={amountChangeHandler} />
                 </div>
                 <div className="form-control">
-                    <label>Date</label>  <input type="date" />
+                    <label>Date</label>  <input type="date" onChange={dateChangeHandler} />
                 </div>
                 <button type="submit">Submit</button>
             </form>
